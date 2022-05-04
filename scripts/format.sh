275b51806bc25ef4daceb5433ee59e36568e15ca
Felix Altenberger <felix@zenml.io>
2022-05-04 14:40:54 +0200

    Include .ipynb notebooks in linting/formatting.

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,9 +2,10 @@
 set -x
 
 SRC=${1:-"src"}
+NOTEBOOKS=${@:-"*.ipynb"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place $SRC --exclude=__init__.py
 isort $SRC
-black $SRC
+black $SRC $NOTEBOOKS
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,11 +3,13 @@ set -e
 set -x
 
 SRC=${1:-"src/"}
+NOTEBOOKS=${@:-"*.ipynb"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
 flake8 $SRC
+flake8-nb $NOTEBOOKS
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place $SRC --exclude=__init__.py --check
 isort $SRC scripts --check-only
-black $SRC  --check
+black $SRC $NOTEBOOKS --check
 interrogate $SRC -c pyproject.toml
